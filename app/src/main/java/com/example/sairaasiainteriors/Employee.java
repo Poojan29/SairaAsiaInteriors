201969d697caddbbd2b46b3444fb4fc89434bb69
Poojan29 <pra.poojan2000@gmail.com>
2020-10-30 12:29:59 +0530

    Commit

---
+++ app/src/main/java/com/example/sairaasiainteriors/Employee.java
@@ -0,0 +1,15 @@
+package com.example.sairaasiainteriors;
+
+public class Employee {
+
+    public String username, email;
+
+    public Employee() {
+    }
+
+    public Employee(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}
